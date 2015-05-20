1bb2d7a8af7dc5d172e21605c916fcc111d90d95
Eric Talevich <eric.talevich@gmail.com>
2015-05-20 16:04:53 -0700

    segmentation: tighten CBS alpha to .0001

--- cnvlib/segmentation/__init__.py
+++ cnvlib/segmentation/__init__.py
@@ -139,8 +139,8 @@ if (is.null(largegaps)) {
 }
 
 write("Segmenting the probe data", stderr())
-fit = segmentByCBS(cna, alpha=0.001, undo=1, joinSegments=FALSE,
-                   knownSegments=knownsegs, seed=0xA5EED)
+fit = segmentByCBS(cna, alpha=.0001, undo=1, min.width=2,
+                   joinSegments=FALSE, knownSegments=knownsegs, seed=0xA5EED)
 
 write("Setting segment endpoints to original bin start/end positions", stderr())
 write("and recalculating segment means with bin weights", stderr())
