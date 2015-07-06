969c31e8e32bedbaa65f82222cff07e068553bc3
Eric Talevich <eric.talevich@gmail.com>
2015-07-06 16:54:28 -0700

    core.sorter_chrom: Make the sort key type-stable (always tuple of int, string)

--- cnvlib/core.py
+++ cnvlib/core.py
@@ -2,6 +2,7 @@
 from __future__ import absolute_import, division, print_function
 import sys
 import os.path
+from itertools import takewhile
 
 from Bio.File import as_handle
 
@@ -39,19 +40,31 @@ def write_tsv(outfname, table, colnames=None):
 # __________________________________________________________________________
 # Sorting key functions
 
-
 def sorter_chrom(label):
     """Create a sorting key from chromosome label.
 
     Sort by integers first, then letters or strings. The prefix "chr"
     (case-insensitive), if present, is stripped automatically for sorting.
 
-    E.g. chr1 < chr2 < chr10 < chrX < chrY
+    E.g. chr1 < chr2 < chr10 < chrX < chrY < chrM
     """
+    # Strip "chr" prefix
     chrom = (label[3:] if label.lower().startswith('chr')
              else label)
-    return (chr(int(chrom)) if chrom.isdigit()
-            else chrom)
+    if chrom in ('X', 'Y'):
+        key = (1000, chrom)
+    else:
+        # Separate numeric and special chromosomes
+        nums = ''.join(takewhile(str.isdigit, chrom))
+        chars = chrom[len(nums):]
+        nums = int(nums) if nums else 0
+        if not chars:
+            key = (nums, '')
+        elif len(chars) == 1:
+            key = (2000 + nums, chars)
+        else:
+            key = (3000 + nums, chars)
+    return key
 
 
 def sorter_chrom_at(index):
