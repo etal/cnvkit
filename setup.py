e57029577f47ee086e80785eaf75952ae8c8a8e9
Eric Talevich <eric.talevich@gmail.com>
2015-08-12 16:34:48 -0700

    setup.py: Use pandas version >= 0.15
    This is the oldest version where Series.searchsorted is available.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ try:
             'numpy >= 1.6',
             'scipy >= 0.9',
             'matplotlib >= 1.1',
-            'pandas >= 0.13',
+            'pandas >= 0.15',
             'pysam >= 0.8',
             'pyvcf >= 0.5',
             'reportlab >= 3.0',
