31978b5eb3ccc4683ca0abb22697ebac80f2cba9
Eric Talevich <eric.talevich@gmail.com>
2015-10-07 17:43:48 -0700

    scatter, loh: Use --normal-id when parsing VCF to filter better
    vary: Store normal sample depths, zygosities, alt counts, alt freqs in the
    parsed dataframe if a normal sample is found in the VCF.

--- cnvlib/vary.py
+++ cnvlib/vary.py
@@ -32,37 +32,41 @@ class VariantArray(gary.GenomicArray):
         if not vcf_reader.samples:
             raise ValueError("No samples found in VCF file " + str(infile))
 
-        sample_id = _select_sample(vcf_reader, sample_id)
-        rows = _parse_records(vcf_reader, sample_id, min_depth, skip_hom,
-                             skip_reject, skip_somatic)
-        table = pd.DataFrame.from_records(rows, columns=[
-            "chromosome", "start", "end", "ref", "alt",
-            "zygosity", "depth", "alt_count"])
+        sample_id, normal_id = _select_sample(vcf_reader, sample_id, normal_id)
+        if normal_id:
+            columns = [
+                "chromosome", "start", "end", "ref", "alt",
+                "zygosity", "depth", "alt_count",
+                "n_zygosity", "n_depth", "n_alt_count"]
+        else:
+            columns = [
+                "chromosome", "start", "end", "ref", "alt",
+                "zygosity", "depth", "alt_count"]
+        rows = _parse_records(vcf_reader, sample_id, normal_id, min_depth,
+                              skip_hom, skip_reject, skip_somatic)
+        table = pd.DataFrame.from_records(rows, columns=columns)
         table["alt_freq"] = table["alt_count"] / table["depth"]
+        if normal_id:
+            table["n_alt_freq"] = table["n_alt_count"] / table["n_depth"]
         return cls(table, {"sample_id": sample_id})
+
 
     # def write_vcf(self, outfile=sys.stdout):
     #     """Write data to a file or handle in VCF format."""
     #     # grab from export.export_vcf()
 
 
-def _select_sample(vcf_reader, sample_id):
+def _select_sample(vcf_reader, sample_id, normal_id):
     """Select a sample ID in the VCF; ensure it's valid."""
-    # ENH - take the paired normal, to select only the tumor records where the
-    # normal sample is het
-    # def get_mutect_tag(metadata):
-    #     for tag in metadata["GATKCommandLine"]:
-    #         if tag["ID"] == "MuTect":
-    #             return sample_id
-
-    if sample_id is None:
+    if sample_id is None and normal_id is None:
         # Use the VCF header to select the tumor sample
         if "PEDIGREE" in vcf_reader.metadata:
             for tag in vcf_reader.metadata["PEDIGREE"]:
                 if "Derived" in tag:
                     sample_id = tag["Derived"]
-                    # normal_id = tag["Original"]
+                    normal_id = tag["Original"]
                     echo("Selected tumor sample", sample_id,
+                         "and normal sample", normal_id,
                          "from the VCF header PEDIGREE tag")
                     break
         elif "GATKCommandLine" in vcf_reader.metadata:
@@ -72,22 +76,42 @@ def _select_sample(vcf_reader, sample_id):
                                    for kv in tag["CommandLineOptions"].split()
                                    if '=' in kv)
                     sample_id = options.get('tumor_sample_name')
-                    # normal_id = options['normal_sample_name=TR_95_N']
+                    normal_id = options['normal_sample_name']
                     echo("Selected tumor sample", sample_id,
+                         "and normal sample", normal_id,
                          "from the MuTect VCF header")
                     break
 
-    if sample_id is None:
+    if sample_id:
+        pass
+        # if normal_id is None and len(vcf_reader.samples == 2):
+        #     normal_id = next(s for s in vcf_reader.samples if s != sample_id)
+    elif normal_id:
+        try:
+            sample_id = next(s for s in vcf_reader.samples if s != normal_id)
+        except StopIteration:
+            raise ValueError("No other sample in VCF besides the specified " +
+                             "normal " + normal_id + "; did you mean to use" +
+                             "this as the sample_id instead?")
+    else:
         sample_id = vcf_reader.samples[0]
-    choices = [s for s in vcf_reader.samples if s == sample_id]
-    assert len(choices) == 1, (
-        "Did not find single sample matching sample id %s: %s"
-        % (sample_id, vcf_reader.samples))
-    return sample_id
+
+    _confirm_unique(sample_id, vcf_reader.samples)
+    if normal_id:
+        _confirm_unique(normal_id, vcf_reader.samples)
+    return sample_id, normal_id
 
 
-def _parse_records(vcf_reader, sample_id, min_depth, skip_hom, skip_reject,
-                  skip_somatic):
+def _confirm_unique(sample_id, samples):
+    occurrences = [s for s in samples if s == sample_id]
+    if len(occurrences) != 1:
+        raise ValueError(
+            "Did not find a single sample ID '%s' in: %s"
+            % (sample_id, samples))
+
+
+def _parse_records(vcf_reader, sample_id, normal_id, min_depth,
+                   skip_hom, skip_reject, skip_somatic):
     """Parse VCF records into DataFrame rows.
 
     Apply filters to skip records with low depth, homozygosity, the REJECT
@@ -106,48 +130,56 @@ def _parse_records(vcf_reader, sample_id, min_depth, skip_hom, skip_reject,
             continue
 
         sample = record.genotype(sample_id)
-
-        # Depth filter
-        if "DP" in sample.data._fields:
-            depth = sample.data.DP
-        else:
-            # SV, probably
-            cnt_depth += 1
-            continue
-        if depth < min_depth:
-            cnt_depth += 1
-            continue
-
-        # Skip homozygous variants (optionally)
-        if sample.is_het:
-            zygosity = 0.5
-        else:
-            if skip_hom:
+        depth, zygosity, alt_count = _extract_genotype(sample)
+        if normal_id:
+            normal = record.genotype(normal_id)
+            n_depth, n_zygosity, n_alt_count = _extract_genotype(normal)
+            if n_depth is None or n_depth < min_depth:
+                cnt_depth += 1
+                continue
+            if skip_hom and n_zygosity  in (0.0, 1.0):
                 cnt_hom += 1
                 continue
-            if sample.gt_type == 0:
-                zygosity = 0.0
-            else:
-                zygosity = 1.0
-
-        alt_count = _get_alt_count(sample)
+        else:
+            if depth is None or depth < min_depth:
+                cnt_depth += 1
+                continue
+            if skip_hom and zygosity  in (0.0, 1.0):
+                cnt_hom += 1
+                continue
 
         # Split multiallelics?
         # XXX Ensure sample genotypes are handled properly
         for alt in record.ALT:
             posn = record.POS - 1
             end = _get_end(posn, alt, record.INFO)
-            yield (record.CHROM,
-                   posn,
-                   end,
-                   record.REF,
-                   str(alt),
-                   zygosity,
-                   depth,
-                   alt_count,
-                  )
+            row = (record.CHROM, posn, end, record.REF, str(alt),
+                zygosity,
+                depth,
+                alt_count,
+                )
+            if normal_id:
+                row += (n_zygosity, n_depth, n_alt_count)
+            yield row
+
     echo("Skipped records:", cnt_reject, "reject,",  cnt_som, "somatic,",
          cnt_depth, "depth,", cnt_hom, "homozygous")
+
+
+def _extract_genotype(sample):
+    if "DP" in sample.data._fields:
+        depth = sample.data.DP
+    else:
+        # SV, probably
+        depth = alt_count = None
+    if sample.is_het:
+        zygosity = 0.5
+    elif sample.gt_type == 0:
+        zygosity = 0.0
+    else:
+        zygosity = 1.0
+    alt_count = _get_alt_count(sample)
+    return depth, zygosity, alt_count
 
 
 def _get_alt_count(sample):
