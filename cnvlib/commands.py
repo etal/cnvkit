e25cdea93e926043ad4588addd0d851fed7d2233
Eric Talevich <eric.talevich@gmail.com>
2016-02-22 22:10:01 -0800

    fix: ensure ratio of target/antitarget IQRs is not 0 or infinity (#82)
    If the majority of target or antitarget bins have no coverage, the IQR range is
    0 and the ratio would be 0 or infinity, causing the antitarget or target bin
    weights to be rescaled all the way to 0 -- which in turn causes CBS to fail. So,
    ensure both IQRs are a bit above 0 when taking the ratio.

--- cnvlib/commands.py
+++ cnvlib/commands.py
@@ -601,14 +601,14 @@ def do_fix(target_raw, antitarget_raw, reference,
         # Down-weight the more variable probe set (targets or antitargets)
         tgt_iqr = metrics.interquartile_range(cnarr.residuals())
         anti_iqr = metrics.interquartile_range(anti_cnarr.residuals())
-        iqr_ratio = tgt_iqr / anti_iqr
+        iqr_ratio = max(tgt_iqr, .01) / max(anti_iqr, .01)
         if iqr_ratio > 1:
             logging.info("Targets are %.2f x more variable than antitargets",
-                        iqr_ratio)
+                         iqr_ratio)
             cnarr["weight"] /= iqr_ratio
         else:
-            logging.info("Antitargets are %.2f x more variable than antitargets",
-                        1. / iqr_ratio)
+            logging.info("Antitargets are %.2f x more variable than targets",
+                         1. / iqr_ratio)
             anti_cnarr["weight"] *= iqr_ratio
         # Combine target and antitarget bins
         cnarr.add(anti_cnarr)
