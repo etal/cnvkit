a0d4038598a0e31f2a5eb5795ccdf68a1987b5cb
Eric Talevich <eric.talevich@gmail.com>
2016-11-14 16:46:28 -0800

    test: split tabio unittests into separate script test_io.py

--- test/test_cnvlib.py
+++ test/test_cnvlib.py
@@ -6,7 +6,6 @@ from __future__ import absolute_import, division, print_function
 import unittest
 
 import numpy as np
-# from Bio._py3k import StringIO
 
 # Import all modules as a smoke test
 import cnvlib
@@ -200,97 +199,6 @@ class CNATests(unittest.TestCase):
             self.assertAlmostEqual(2, resid.std(), delta=.5)
 
     # def test_squash_genes(self):
-
-
-
-class IOTests(unittest.TestCase):
-    """Tests for I/O modules."""
-
-    def test_empty(self):
-        """Instantiate from an empty file."""
-        for fmt in ("auto", "bed", #"bed3", "bed4",
-                    "interval", "tab", "text"):
-            regions = tabio.read("formats/empty", fmt=fmt)
-            self.assertEqual(len(regions), 0)
-
-    def test_read_auto(self):
-        for fname, nrows in (("formats/empty", 0),
-                             ("formats/amplicon.bed",
-                              linecount("formats/amplicon.bed")),
-                              ("formats/nv2_baits.interval_list", 6809)):
-            self.assertEqual(len(tabio.read_auto(fname)), nrows)
-            with open(fname) as handle:
-                self.assertEqual(len(tabio.read_auto(handle)), nrows)
-
-    def test_read_bed(self):
-        """Read the BED format."""
-        fname = "formats/amplicon.bed"
-        regions = tabio.read(fname, "bed")
-        self.assertEqual(len(regions), linecount(fname))
-        self.assertEqual(regions.sample_id, "amplicon")
-
-    def test_read_ilist(self):
-        """Read the interval list format."""
-        regions = tabio.read("formats/nv2_baits.interval_list", "interval")
-        self.assertEqual(len(regions), 6809)
-        self.assertEqual(regions.sample_id, "nv2_baits")
-
-    def test_read_picardhs(self):
-        """Read Picard CalculateHsMetrics PER_TARGET_COVERAGE format."""
-        fname = "picard/p2-5_5.antitargetcoverage.csv"
-        cna = tabio.read(fname, "picardhs")
-        self.assertEqual(len(cna), linecount(fname) - 1)
-        self.assertEqual(cna.sample_id, "p2-5_5")
-
-    def test_read_seg(self):
-        """Read the SEG format."""
-        for fname, header_len, args in (
-            # Convert integer chrom. IDs to hg19 names
-            ('formats/cw-tr-log2.seg', 1,
-             ({'23': 'X', '24': 'Y', '25': 'M'}, "chr", False)),
-            # Convert segmented array CGH data in log10 scale to log2
-            ('formats/acgh-log10.seg', 1, (None, None, True)),
-            # From PSCBS/DNAcopy, with a stray warning message from R
-            ('formats/warning.seg', 2, (None, None, False)),
-        ):
-            expect_lines = linecount(fname) - header_len
-            seen_lines = 0
-            for _sample_id, dframe in tabio.seg.parse_seg(fname, *args):
-                seen_lines += len(dframe)
-            self.assertEqual(seen_lines, expect_lines)
-
-    def test_read_text(self):
-        """Read the text region format."""
-        fname = "formats/amplicon.text"
-        regions = tabio.read(fname, "text")
-        self.assertEqual(len(regions), linecount(fname))
-        self.assertEqual(regions.sample_id, "amplicon")
-
-    def test_read_vcf(self):
-        """Read the VCF format."""
-        # Paired VCF with full info
-        fname = "formats/na12878_na12882_mix.vcf"
-        v1 = tabio.read(fname, "vcf")
-        self.assertLess(len(v1), linecount(fname))
-        self.assertLess(0, len(v1))
-        for sid in ("NA12882", "NA12878"):
-            v2 = tabio.read(fname, "vcf", sample_id=sid)
-            self.assertEqual(v2.sample_id, sid)
-            self.assertEqual(len(v1), len(v2))
-        for kwarg in ({'min_depth': 100},
-                      {'skip_somatic': True},
-                      {'skip_reject': True}):
-            v3 = tabio.read(fname, "vcf", **kwarg)
-            self.assertLess(len(v3), len(v1))
-            self.assertLess(0, len(v3))
-        # VCF header, no samples, no records
-        v4 = tabio.read('formats/nosample.vcf', 'vcf')
-        self.assertEqual(len(v4), 0)
-        self.assertEqual(v4.sample_id, 'nosample')
-        # VCF with 1 sample, no records
-        v5 = tabio.read('formats/blank.vcf', 'vcf', sample_id='Blank')
-        self.assertEqual(len(v5), 0)
-        self.assertEqual(v5.sample_id, 'Blank')
 
 
 
