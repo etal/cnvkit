99d77e66099f554ca8d44c93929fc1a5feca4ccb
Eric Talevich <eric.talevich@gmail.com>
2016-12-31 11:57:36 -0800

    py3...

--- cnvlib/coverage.py
+++ cnvlib/coverage.py
@@ -1,6 +1,6 @@
 """Supporting functions for the 'antitarget' command."""
 from __future__ import absolute_import, division, print_function
-from builtins import zip
+from builtins import str, zip
 from past.builtins import basestring
 
 import atexit
@@ -203,7 +203,7 @@ def bedcov(bed_fname, bam_fname, min_mapq):
         raise ValueError("BED file %r chromosome names don't match any in "
                          "BAM file %r" % (bed_fname, bam_fname))
     columns = detect_bedcov_columns(raw)
-    table = pd.read_table(StringIO(unicode(raw)), names=columns, usecols=columns)
+    table = pd.read_table(StringIO(str(raw)), names=columns, usecols=columns)
     if 'gene' in table:
         table['gene'] = table['gene'].fillna('-')
     else:
