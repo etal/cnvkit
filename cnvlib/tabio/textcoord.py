f4c28fb0295fd49bc0808e24e1501d6bea83a473
Eric Talevich <eric.talevich@gmail.com>
2017-02-08 17:35:34 -0800

    tabio.textcoord: expose functions from_label, to_label

--- cnvlib/tabio/textcoord.py
+++ cnvlib/tabio/textcoord.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, division, print_function
+import collections
 
 import pandas as pd
 from Bio.File import as_handle
@@ -14,23 +15,30 @@ def read_text(infile):
 
     Coordinate indexing is assumed to be from 1.
     """
-    @report_bad_line
-    def _parse_line(line):
-        fields = line.split(':')
-        if len(fields) == 3:
-            chrom, start_end, gene = fields
-        elif len(fields) == 2:
-            chrom, start_end = fields
-            gene = '-'
-        else:
-            raise ValueError("Bad line: %r" % line)
-        start, end = start_end.split('-')
-        return chrom, int(start) - 1, int(end), gene.rstrip()
-
+    parse_line = report_bad_line(from_label)
     with as_handle(infile, 'rU') as handle:
-        rows = [_parse_line(line) for line in handle]
+        rows = [parse_line(line) for line in handle]
     return pd.DataFrame.from_records(rows, columns=["chromosome", "start",
                                                     "end", "gene"])
+
+
+Region = collections.namedtuple('Region', 'chromosome start end gene')
+
+def from_label(label):
+    fields = label.split(':')
+    if len(fields) == 3:
+        chrom, start_end, gene = fields
+    elif len(fields) == 2:
+        chrom, start_end = fields
+        gene = '-'
+    else:
+        raise ValueError("Bad label: %r" % label)
+    start, end = start_end.split('-')
+    return Region(chrom, int(start) - 1, int(end), gene.rstrip())
+
+# ENH: import from_label, to_label at top level in tabio.__init__?
+# - move GA.row2label implementation here? Both import it from core/util?
+to_label = GA.row2label
 
 
 def write_text(dframe):
