f4417e023583b2c4d65f4bf231077141f10dd264
Eric Talevich <eric.talevich@gmail.com>
2017-04-28 15:48:29 -0700

    export theta: don't crash on bins where reference is NaN (#168)

--- cnvlib/export.py
+++ cnvlib/export.py
@@ -425,7 +425,7 @@ def theta_read_counts(log2_ratio, nbins,
     """
     read_depth = (2 ** log2_ratio) * avg_depth
     read_count = nbins * avg_bin_width * read_depth / read_len
-    return read_count.round().astype('int')
+    return read_count.round().fillna(0).astype('int')
 
 
 def export_theta_snps(varr):
