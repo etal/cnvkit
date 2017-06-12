cbdd2e103cc2a5949c0b312b6151c84f79bc658c
Eric Talevich <eric.talevich@gmail.com>
2017-06-12 16:26:12 -0700

    export cdt: Fix swapped columns in output; add "EWIEGHT" row for compat
    Build dataframes from a list of columns & value arrays instead of a
    dict, which was ignoring order and causing column values to disagree
    with the header. (Reported by HST)
    
    Also add a dummy EWEIGHT row, per online specs, to ensure Java TreeView
    starts reading data rows from the correct row. Not sure if this was ever
    a problem in any JTV versions.

--- cnvlib/export.py
+++ cnvlib/export.py
@@ -49,19 +49,26 @@ def merge_samples(filenames):
 # Supported formats:
 
 def fmt_cdt(sample_ids, table):
-    """Format as CDT."""
+    """Format as CDT.
+
+    See:
+
+    - http://jtreeview.sourceforge.net/docs/JTVUserManual/ch02s11.html
+    - http://www.eisenlab.org/FuzzyK/cdt.html
+    """
     outheader = ['GID', 'CLID', 'NAME', 'GWEIGHT'] + sample_ids
     header2 = ['AID', '', '', '']
     header2.extend(['ARRY' + str(i).zfill(3) + 'X'
                     for i in range(len(sample_ids))])
-    outrows = [header2]
+    header3 = ['EWEIGHT', '', '', ''] + ['1'] * len(sample_ids)
+    outrows = [header2, header3]
     outtable = pd.concat([
-        pd.DataFrame({
-            "GID": pd.Series(table.index).apply(lambda x: "GENE%dX" % x),
-            "CLID": pd.Series(table.index).apply(lambda x: "IMAGE:%d" % x),
-            "NAME": table["label"],
-            "GWEIGHT": 1,
-        }),
+        pd.DataFrame.from_items([
+           ("GID", pd.Series(table.index).apply(lambda x: "GENE%dX" % x)),
+           ("CLID", pd.Series(table.index).apply(lambda x: "IMAGE:%d" % x)),
+           ("NAME", table["label"]),
+           ("GWEIGHT", 1),
+        ]),
         table.drop(["chromosome", "start", "end", "gene", "label"],
                    axis=1)],
         axis=1)
