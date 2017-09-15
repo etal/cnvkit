c264a73e401d5d030ee7ca510e2db7004418b469
Eric Talevich <eric.talevich@gmail.com>
2017-09-15 15:15:29 -0700

    scripts/cn_ztest.py: Options to account for known segs, sex chroms
    Refactored to fit the style of commands.py

--- scripts/cn_ztest.py
+++ scripts/cn_ztest.py
@@ -10,10 +10,59 @@ import numpy as np
 from scipy.stats import norm
 
 import cnvlib
+from cnvlib.cmdutil import verify_sample_sex
 from skgenome import tabio
 
 
-def ztest(cnarr, alpha, verbose=True):
+def _cmd_ztest(args):
+    cnarr = cnvlib.read(args.cnarr)
+    if args.segment:
+        segments = cnvlib.read(args.segment)
+        is_sample_female = None
+    else:
+        segments = None
+        is_sample_female = verify_sample_sex(cnarr, args.sample_sex,
+                                             args.male_reference)
+    sig = do_ztest(cnarr, segments, args.male_reference, is_sample_female,
+                   args.alpha, args.target)
+    if len(sig):
+        tabio.write(sig, args.output or sys.stdout)
+
+
+def do_ztest(cnarr, segments=None, is_male_reference=False,
+             is_sample_female=None, alpha=0.005, target_only=False):
+    if segments:
+        # Subtract segment means to report only the CNA bins that weren't
+        # already detected (including exon-size CNAs within a larger-scale,
+        # smaller-amplitude CNA)
+        cnarr['log2'] = cnarr.residuals(segments)
+    else:
+        # Account for non-diploid sex chromosomes
+        cnarr['log2'] -= cnarr.expect_flat_log2(is_male_reference)
+        if is_sample_female:
+            # chrX has same ploidy as autosomes; chrY is just unusable noise
+            cnarr = cnarr[cnarr.chromosome != cnarr._chr_y_label]
+        else:
+            # 1/2 #copies of each sex chromosome
+            is_chr_xy = cnarr.chromosome.isin((cnarr._chr_x_label,
+                                               cnarr._chr_y_label))
+            cnarr[is_chr_xy, 'log2'] += 1.0
+
+    if target_only:
+        antitarget_idx = cnarr['gene'].isin(cnvlib.params.ANTITARGET_ALIASES)
+        if antitarget_idx.any():
+            print("Ignoring", antitarget_idx.sum(), "off-target bins")
+            cnarr = cnarr[~antitarget_idx]
+
+    cnarr['ztest'] = z_prob(cnarr)
+    is_sig = cnarr['ztest'] < alpha
+    logging.info("Significant hits in {}/{} bins ({:.3g}%)"
+                 .format(is_sig.sum(), len(is_sig),
+                         100 * is_sig.sum() / len(is_sig)))
+    return cnarr[is_sig]
+
+
+def z_prob(cnarr):
     """Get a probability for each bin based on its Z-score.
 
     Return bins where the probability < `alpha`.
@@ -22,15 +71,7 @@ def ztest(cnarr, alpha, verbose=True):
     sd = np.sqrt(1 - cnarr['weight'])
     p = norm.pdf(cnarr['log2'], loc=0, scale=sd)
     # Correct for multiple hypothesis tests
-    # p *= len(p)       # Bonferroni
-    p = p_adjust_bh(p)  # B-H
-    cnarr['ztest'] = p
-
-    is_sig = cnarr['ztest'] < alpha
-    if verbose:
-        print("Significant hits in", is_sig.sum(), "/", len(is_sig), "bins",
-              "({:.4g}%)".format(100 * is_sig.sum() / len(is_sig)))
-    return cnarr[is_sig]
+    return p_adjust_bh(p)
 
 
 def p_adjust_bh(p):
@@ -43,27 +84,30 @@ def p_adjust_bh(p):
     return q[by_orig]
 
 
+
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, format="%(message)s")
 
     AP = argparse.ArgumentParser(description=__doc__)
     AP.add_argument("cnarr", help=".cnr file")
     AP.add_argument("-a", "--alpha", type=float, default=0.005,
-                    help="Significance threhold")
+                    help="Significance threhold. [Default: %(default)s]")
     AP.add_argument("-t", "--target", action="store_true",
                     help="Test target bins only; ignore off-target bins.")
+
+    AP.add_argument('-s', '--segment', metavar="FILENAME",
+                    help="""Segmentation calls (.cns), the output of the
+                    'segment' command).""")
+    AP.add_argument('-y', '--male-reference', action='store_true',
+                    help="""Assume inputs were normalized to a male reference
+                    (i.e. female samples will have +1 log-coverage of chrX;
+                    otherwise male samples would have -1 chrX).""")
+    AP.add_argument('-x', '--sample-sex',
+                    choices=('m', 'y', 'male', 'Male',
+                             'f', 'x', 'female', 'Female'),
+                    help="""Specify the sample's chromosomal sex as male or
+                    female.  (Otherwise guessed from X and Y coverage).""")
+
     AP.add_argument("-o", "--output",
                     help="Output filename.")
-    args = AP.parse_args()
-
-    cnarr = cnvlib.read(args.cnarr)
-
-    if args.target:
-        antitarget_idx = cnarr['gene'].isin(cnvlib.params.ANTITARGET_ALIASES)
-        if antitarget_idx.any():
-            print("Ignoring", antitarget_idx.sum(), "off-target bins")
-            cnarr = cnarr[~antitarget_idx]
-
-    sig = ztest(cnarr, args.alpha)
-    if len(sig):
-        tabio.write(sig, args.output or sys.stdout)
+    _cmd_ztest(AP.parse_args())
