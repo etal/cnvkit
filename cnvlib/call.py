c56ec23ca9cd1406d705fc50364b2863c002e4a6
Eric Talevich <eric.talevich@gmail.com>
2017-11-20 14:24:52 -0800

    Fix formatting of a docstring

--- cnvlib/call.py
+++ cnvlib/call.py
@@ -256,23 +256,25 @@ def _log2_ratio_to_absolute(log2_ratio, ref_copies, expect_copies, purity=None):
 
     Does not round to an integer absolute value here.
 
-    Math:
+    Math::
 
         log2_ratio = log2(ncopies / ploidy)
         2^log2_ratio = ncopies / ploidy
         ncopies = ploidy * 2^log2_ratio
 
-    With rescaling for purity:
+    With rescaling for purity::
 
         let v = log2 ratio value, p = tumor purity,
-            r = reference ploidy, x = expected ploidy;
+            r = reference ploidy, x = expected ploidy,
+            n = tumor ploidy ("ncopies" above);
+
         v = log_2(p*n/r + (1-p)*x/r)
         2^v = p*n/r + (1-p)*x/r
         n*p/r = 2^v - (1-p)*x/r
         n = (r*2^v - x*(1-p)) / p
 
     If purity adjustment is skipped (p=1; e.g. if germline or if scaling for
-    heterogeneity was done beforehand):
+    heterogeneity was done beforehand)::
 
         n = r*2^v
     """
