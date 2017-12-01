cec6b2cd19139a836a4be75aa31a7026f3f55581
Eric Talevich <eric.talevich@gmail.com>
2017-12-01 15:03:03 -0800

    Add 2 scripts to infer copy number from RNA expression
    import_gene_counts.py: TCGA 2-column format (expression medians)
    import_rsem.py: RSEM "*.gene.results" multi-column format
    
    Supported by new module cnvlib.rna

---
+++ cnvlib/rna.py
@@ -0,0 +1,420 @@
+"""RNA expression levels quantified per gene.
+
+Process per-gene expression levels, or the equivalent, by cohort.
+"""
+from __future__ import absolute_import, division, print_function
+from builtins import zip
+
+import logging
+import sys
+import warnings
+
+import numpy as np
+import pandas as pd
+from scipy.stats import spearmanr, kendalltau, gmean
+
+from .cnary import CopyNumArray as CNA
+from .fix import center_by_window
+from .params import NULL_LOG2_COVERAGE
+from .descriptives import biweight_midvariance
+
+
+def before(char):
+    def selector(string):
+        return string.split(char, 1)[0]
+    return selector
+
+
+def filter_probes(sample_counts):
+    """Filter probes to only include high-quality, transcribed genes.
+
+    The human genome has ~25,000 protein coding genes, yet the RSEM output
+    includes ~58,000 rows. Some of these rows correspond to read counts over
+    control probes (e.g.  spike-in sequences). Some rows correspond to poorly
+    mapped genes in contigs that have not been linked to the 24 chromosomes
+    (e.g. HLA region). Others correspond to pseudo-genes and non-coding genes.
+    For the purposes of copy number inference, these rows are best removed.
+    """
+    gene_medians = sample_counts.median(axis=1)
+    ## Make sure the gene has detectable transcript in at least half of samples
+    is_mostly_transcribed = (gene_medians >= 1.0)
+    print("Dropping", (~is_mostly_transcribed).sum(), "/",
+          len(is_mostly_transcribed), "rarely expressed genes from input samples")
+    return sample_counts[is_mostly_transcribed]
+
+
+def load_gene_info(gene_resource, tcga_cnv, tcga_expression, default_r=.1):
+    """Read gene info from BioMart, and optionally TCGA, into a dataframe.
+
+    RSEM only outputs the Ensembl ID. We have used the BioMART tool in Ensembl
+    to export a list of all Ensembl genes with a RefSeq mRNA (meaning it is high
+    quality, curated, and bona fide gene) and resides on chromosomes 1-22, X, or
+    Y. The tool also outputs the GC content of the gene, chromosomal coordinates
+    of the gene, and HUGO gene symbol.
+
+    The gene resource input can be obtained from a resource bundle we provide
+    (for reference genome hg19) or generated from BioMart.
+    """
+    # Load the gene info file and clean up column names
+    # Original columns:
+    # "Gene stable ID" -- Ensembl ID
+    # "Gene % GC content"
+    # "Chromosome/scaffold name"
+    # "Gene start (bp)"
+    # "Gene end (bp)"
+    # "Gene name"
+    # "NCBI gene ID"
+    # "Transcript length (including UTRs and CDS)"
+    # "Transcript support level (TSL)"
+
+    # DBG
+    #  gene_resource = "../GeneInfo/GeneInfo3_mart_export.txt"
+    #  tcga_cnv = "../Get_PearsonValue/InputFiles/data_CNA.txt"
+    #  tcga_expression = "../Get_PearsonValue/InputFiles/data_RNA_Seq_v2_expression_median.txt"
+
+    info_col_names = ['gene_id', 'gc', 'chromosome', 'start', 'end',
+                      'gene', 'entrez_id', 'tx_length', 'tx_support']
+    gene_info = (pd.read_table(gene_resource, names=info_col_names, header=1,
+                               converters={'gene_id': before('.'),
+                                           'tx_support': tsl2int,
+                                           'gc': lambda x: float(x)/100})
+                 .sort_values('gene_id'))
+    print("Loaded", gene_resource, "with shape:", gene_info.shape,
+          file=sys.stderr)
+
+    if tcga_cnv and tcga_expression:
+        corr_table = correlate_cnv_expression(tcga_cnv, tcga_expression)
+
+        gi_corr = gene_info.join(corr_table, on='entrez_id', how='left')
+        if not gi_corr['gene_id'].is_unique:
+            unique_idx = gi_corr.groupby('gene_id').apply(dedupe_ens_hugo)
+            gi_corr = gi_corr.loc[unique_idx]
+
+        if not gene_info['entrez_id'].is_unique:
+            # Treat untraceable entrez_id duplicates (scarce at this point, ~15)
+            # as unknown correlation, i.e. default, as if not in TCGA
+            gi_corr.loc[tuple(locate_entrez_dupes(gi_corr)),
+                        ('pearson_r', 'spearman_r', 'kendall_t')] = default_r
+
+        # Genes w/o TCGA info get default correlation 0.1 (= .5*corr.median())
+        gene_info = gi_corr.fillna({'pearson_r': default_r,
+                                    'spearman_r': default_r,
+                                    'kendall_t': default_r,
+                                   })
+
+    elif not gene_info['gene_id'].is_unique:
+        # Simpler deduplication, not using TCGA fields
+        unique_idx = gene_info.groupby('gene_id').apply(dedupe_ens_no_hugo)
+        gene_info = gene_info.loc[unique_idx]
+
+    print("Trimmed gene info table to shape:", gene_info.shape)
+    assert gene_info['gene_id'].is_unique
+    gene_info['entrez_id'] = gene_info['entrez_id'].fillna(0).astype('int')
+    return gene_info.set_index('gene_id')
+
+
+
+def correlate_cnv_expression(cnv_fname, expression_fname):
+    """Get correlation coefficients for matched copy number and expression data.
+
+    cBioPortal offers a nice feature in which you can download a summary of many
+    large-scale sequencing studies. In this summary are two files that contain
+    the copy number and expression values of every gene in the study for every
+    sample.  This summary is available for nearly every TCGA study, and the data
+    is intuitive to access, therefore I have designed this pre-processing script
+    to accept these as inputs. Of course, the user can calculate their own
+    Pearson values from other sources of data if they prefer -- in this case,
+    the user should formate their data to match the output of this prepocessing
+    script.
+    """
+    shared_key = 'Entrez_Gene_Id'
+    cnv_table = load_tcga_table(cnv_fname, shared_key)
+    expr_table = load_tcga_table(expression_fname, shared_key)
+    # Ensure rows and columns match
+    cnv_table, expr_table = cnv_table.align(expr_table, join='inner')
+    print("Trimmed TCGA tables to shape:", cnv_table.shape)
+
+    # Calculate correlation coefficients
+    print("Calculating correlation coefficients")
+    c_nums = cnv_table._get_numeric_data()
+    e_nums = expr_table._get_numeric_data()
+    # Pearson correlation coefficient (superfast)
+    r = c_nums.corrwith(e_nums, axis=1)
+    # Spearman, Kendall (slow)
+    # NB: RuntimeWarning from numpy/scipy for divide-by-zero / inf / nan
+    # ENH: with warnings.catch_warnings(): but just catch RuntimeWarning
+    warnings.simplefilter('ignore', RuntimeWarning)
+    rho, tau = zip(*[
+        (spearmanr(cnv_row, expr_row, nan_policy='omit')[0],
+         kendalltau(cnv_row, expr_row, nan_policy='omit')[0])
+        for cnv_row, expr_row in zip(c_nums.values, e_nums.values)])
+
+    result = pd.DataFrame({
+        "pearson_r": r.values,
+        "spearman_r": rho,
+        "kendall_t": tau,
+    }, index=cnv_table.index).clip_lower(0).fillna(0)
+    result["hugo_gene"] = cnv_table['Hugo_Symbol'].fillna('').values
+    return result
+
+
+def load_tcga_table(fname, shared_key):
+    """Load TCGA expression/CNV data, keeping unique Entrez genes.
+
+    Rows without an Entrez_Gene_Id value are dropped. Where a gene has multiple
+    HUGO names but one Entrez_Gene_Id (i.e. multiple rows with the same
+    Entrez_Gene_Id), only the sortest and then alphabetically first HUGO name is
+    kept, ensuring Entrez_Gene_Id values are unique.
+    """
+    table = pd.read_table(fname, dtype={shared_key: str}, na_filter=False)
+    table = table[table[shared_key] != ''].astype({shared_key: int})
+    before_pipe = before('|')
+    sort_order = (table['Hugo_Symbol']
+                  .apply(lambda x: (len(x), before_pipe(x)))
+                  .argsort())
+    table = (table.iloc[sort_order]
+             .drop_duplicates(subset=shared_key)
+             .set_index(shared_key)
+             .sort_index(axis=0)
+             .sort_index(axis=1))
+    print("Loaded", fname, "with shape:", table.shape, file=sys.stderr)
+    return table
+
+
+def tsl2int(tsl):
+    """Convert an Ensembl Transcript Support Level (TSL) code to an integer.
+
+    The code has the format "tsl([1-5]|NA)".
+
+    See: https://www.ensembl.org/Help/Glossary?id=492
+    """
+    if tsl in (np.nan, "", "tslNA"):
+        return 0
+    # Remove "tsl" prefix
+    assert tsl[:3] == "tsl"
+    value = tsl[3:]
+    # Remove possible suffix " (assigned to previous version _)"
+    if len(value) > 2:
+        value = value[:2].rstrip()
+    if value == "NA":
+        return 0
+    return int(value)
+
+
+def dedupe_ens_hugo(dframe):
+    """Emit the "best" index from a group of the same Ensembl ID.
+
+    The RSEM gene rows are the data of interest, and they're associated with
+    Ensembl IDs to indicate the transcribed gene being measured in each row.
+    The BioMart table of gene info can have duplicate rows for Ensembl ID, which
+    would cause duplicate rows in RSEM import if joined naively.  So, we need to
+    select a single row for each group of "gene info" rows with the same Ensembl
+    ID (column 'gene_id').
+
+    The keys we can use for this are:
+
+    - Entrez ID ('entrez_id')
+    - Ensembl gene name ('gene')
+    - Entrez gene name ('hugo_gene')
+
+    Entrez vs. Ensembl IDs and gene names are potentially many-to-many, e.g.
+    CALM1/2/3. However, if we also require that the Ensembl and HUGO gene names
+    match within a group, that (usually? always?) results in a unique row
+    remaining.
+
+    (Example: CALM1/2/3 IDs are many-to-many, but of the 3 Entrez IDs associated
+    with Ensembl's CALM1, only 1 is called CALM1 in the Entrez/corr. table.)
+
+    Failing that (no matches or multiple matches), prefer a lower Entrez ID,
+    because well-characterized, protein-coding genes tend to have been
+    discovered and accessioned first.
+    """
+    if len(dframe) == 1:
+        return dframe.index[0]
+    match_gene = dframe[dframe['gene'] == dframe['hugo_gene']]
+    if len(match_gene) == 1:
+        return match_gene.index[0]
+    if len(match_gene) > 1:
+        # Take lowest Entrez ID of the matched
+        return dedupe_tx(match_gene)
+    # No matching names -> lowest Entrez ID key
+    return dedupe_tx(dframe)
+
+
+def dedupe_ens_no_hugo(dframe):
+    """Deduplicate Ensembl ID using Entrez ID but not HUGO gene name."""
+    if len(dframe) == 1:
+        return dframe.index[0]
+    return dedupe_tx(dframe)
+
+
+def dedupe_tx(dframe):
+    """Deduplicate table rows to select one transcript length per gene.
+
+    Choose the lowest-number Entrez ID and the transcript with the greatest
+    support (primarily) and length (secondarily).
+
+    This is done at the end of Ensembl ID deduplication, after filtering on gene
+    names and for single-row tables.
+
+    Returns an integer row index corresponding to the original table.
+    """
+    # NB: Transcripts are many-to-1 vs. Ensembl ID, not Entrez ID, so each
+    # unique Entrez ID here will have the same collection of transcripts
+    # associated with it.
+    return (dframe.sort_values(['entrez_id', 'tx_support', 'tx_length'],
+                               ascending=[True, False, False],
+                               na_position='last')
+            .index[0])
+
+
+def locate_entrez_dupes(dframe):
+    """In case the same Entrez ID was assigned to multiple Ensembl IDs.
+
+    Use HUGO vs. HGNC name again, similar to `dedupe_hugo`, but instead of
+    emiting the indices of the rows to keep, emit the indices of the extra rows
+    -- their correlation values will then be filled in with a default value
+    (np.nan or 0.1).
+
+    It will then be as if those genes hadn't appeared in the TCGA tables at all,
+    i.e. CNV-expression correlation is unknown, but all entries are still
+    retained in the BioMart table (gene_info).
+    """
+    for _key, group in dframe.groupby('entrez_id'):
+        if len(group) == 1:
+            continue
+        match_gene_idx = (group['gene'] == group['hugo_gene'])
+        match_gene_cnt = match_gene_idx.sum()
+        if match_gene_cnt == 1:
+            for mismatch_idx in group.index[~match_gene_idx]:
+                yield mismatch_idx
+        else:
+            # Keep the lowest Ensemble ID (of the matched, if any)
+            if match_gene_cnt: # >1
+                keepable = group[match_gene_idx]
+            else:
+                keepable = group
+            idx_to_keep = keepable.sort_values('gene_id').index.values[0]
+            for idx in group.index:
+                if idx != idx_to_keep:
+                    yield idx
+
+
+def align_gene_info_to_samples(gene_info, sample_counts, tx_lengths):
+    """Align columns and sort.
+
+    Also calculate weights and add to gene_info as 'weight', along with
+    transcript lengths as 'tx_length'.
+    """
+    print("Dimensions: gene_info=%s, sample_counts=%s"
+          % (gene_info.shape, sample_counts.shape))
+    sc, gi = sample_counts.align(gene_info, join='inner', axis=0)
+    gi = gi.sort_values(by=['chromosome', 'start'])
+    sc = sc.loc[gi.index]
+
+    if tx_lengths is not None:
+        # Replace the existing tx_lengths from gene_resource
+        # (RSEM results have this, TCGA gene counts don't)
+        gi['tx_length'] = tx_lengths.loc[gi.index]
+
+    # Calculate per-gene weights similarly to cnvlib.fix
+    # NB: chrX doesn't need special handling because with X-inactivation,
+    # expression should be similar in male and female samples, i.e. neutral is 0
+    logging.info("Weighting genes with below-average read counts")
+    gene_counts = sc.median(axis=1)
+    weights = [np.sqrt((gene_counts / gene_counts.quantile(.75)).clip_upper(1))]
+
+    logging.info("Calculating normalized gene read depths")
+    sample_depths_log2 = normalize_read_depths(sc.divide(gi['tx_length'], axis=0))
+
+    logging.info("Weighting genes by spread of read depths")
+    gene_spreads = sample_depths_log2.apply(biweight_midvariance, axis=1)
+    weights.append(gene_spreads)
+
+    corr_weights = []
+    for corr_col in ('spearman_r', 'pearson_r', 'kendall_t'):
+        if corr_col in gi:
+            logging.info("Weighting genes by %s correlation coefficient",
+                         corr_col)
+            corr_weights.append(gi[corr_col].values)
+    if corr_weights:
+        weights.append(np.vstack(corr_weights).mean(axis=0))
+
+    weight = gmean(np.vstack(weights), axis=0)
+    gi['weight'] = weight / weight.max()
+    if gi['weight'].isnull().all():
+        gi['weight'] = 1.0
+    logging.info(" --> final zeros: %d / %d",
+                 (gi['weight'] == 0).sum(), len(gi))
+    return gi, sc, sample_depths_log2
+
+
+def normalize_read_depths(sample_depths):
+    """Normalize read depths within each sample.
+
+    Some samples have higher sequencing depth and therefore read depths need to
+    be normalized within each sample. TCGA recommends an upper quartile
+    normalization.
+
+    After normalizing read depths within each sample, normalize (median-center)
+    within each gene, across samples.
+
+    Finally, convert to log2 ratios.
+    """
+    assert sample_depths.values.sum() > 0
+    for _i in range(4):
+        # By-sample: 75%ile  among all genes
+        q3 = sample_depths.quantile(.75)
+        sample_depths /= q3
+        # By-gene: median among all samples
+        sm = sample_depths.median(axis=1)
+        sample_depths = sample_depths.divide(sm, axis=0)
+        # print("  round", _i, "grand mean:", sample_depths.values.mean(),
+        #       ": sample-wise denom =", q3.mean(),
+        #       ", gene-wise denom =", sm.mean())
+    # Finally, convert normalized read depths to log2 scale
+    return np.log2(sample_depths).replace(-np.inf, np.nan)
+
+
+def attach_gene_info_to_cnr(sample_counts, sample_data_log2, gene_info,
+                            read_len=100):
+    """Join gene info to each sample's log2 expression ratios.
+
+    Add the Ensembl gene info to the aggregated gene expected read counts,
+    dropping genes that are not in the Ensembl table
+    I.e., filter probes down to those genes that have names/IDs in the gene
+    resource table.
+
+    Split out samples to individual .cnr files, keeping (most) gene info.
+    """
+    gi_cols = ['chromosome', 'start', 'end', 'gene', 'gc', 'tx_length', 'weight']
+    cnr_info = gene_info.loc[:, gi_cols]
+    # Fill NA fields with the lowest finite value in the same row.
+    # Only use NULL_LOG2_COVERAGE if all samples are NA.
+    gene_minima = sample_data_log2.min(axis=1, skipna=True).fillna(NULL_LOG2_COVERAGE)
+    for (sample_id, sample_col), (_sid_log2, sample_log2) \
+            in zip(sample_counts.iteritems(), sample_data_log2.iteritems()):
+        tx_len = cnr_info.tx_length
+        sample_depth = (read_len * sample_col / tx_len).rename("depth")
+        sample_log2 = sample_log2.fillna(gene_minima).rename("log2")
+        cdata = (pd.concat([cnr_info, sample_depth, sample_log2], axis=1)
+                 .reset_index(drop=True))
+        cnr = CNA(cdata, {'sample_id': sample_id})
+        cnr.sort_columns()
+        yield cnr
+
+
+def correct_cnr(cnr):
+    """Apply bias corrections & smoothing.
+
+    - Biases: 'gc', 'length'
+    - Smoothing: rolling triangle window using weights.
+    """
+    cnr.center_all()
+    # Biases, similar to stock CNVkit
+    if 'gc' in cnr:
+        cnr = center_by_window(cnr, .1, cnr['gc'])
+    if 'tx_length' in cnr:
+        cnr = center_by_window(cnr, .1, cnr['tx_length'])
+    cnr.center_all()
+    return cnr
---
+++ scripts/import_rsem.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+"""Extract CNVkit-compatible per-gene, per-sample log2 ratios from RSEM outputs.
+"""
+from __future__ import absolute_import, division, print_function
+
+import logging
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+from skgenome import tabio
+
+from cnvlib import rna
+
+
+def aggregate_rsem(fnames):
+    """Pull out the expected read counts from each RSEM file.
+
+    The expected read counts are located in the fifth column of this version of
+    RSEM output. They start on the second line (after a header line). I will
+    also pull the ensembl gene id, which is located in the first column.
+
+    Returns:
+        sample_counts : DataFrame
+            Row index is Ensembl gene ID, column index is filename.
+        tx_lengths : Series
+            Gene lengths.
+    """
+    prev_row_count = None
+    sample_cols = {}
+    length_cols = []
+    length_colname = 'length'  # or: 'effective_length'
+    for fname in fnames:
+        # NB: read_table(index_col=_) works independently of combine=, dtype=
+        #   so index column needs to be processed separately
+        #   https://github.com/pandas-dev/pandas/issues/9435
+        d = pd.read_table(fname,
+                          usecols=['gene_id', length_colname, 'expected_count'],
+                          #  index_col='gene_id',
+                          converters={'gene_id': rna.before('.')}
+                         ).set_index('gene_id')
+        if prev_row_count is None:
+            prev_row_count = len(d)
+        elif len(d) != prev_row_count:
+            raise RuntimeError("Number of rows in each input file is not equal")
+        sample_id = rna.before(".")(os.path.basename(fname))
+        sample_cols[sample_id] = d.expected_count.fillna(0)
+        length_cols.append(d[length_colname])
+    sample_counts = pd.DataFrame(sample_cols)
+    tx_lengths = pd.Series(np.vstack(length_cols).mean(axis=0),
+                             index=sample_counts.index)
+    return sample_counts, tx_lengths
+
+
+def main(args):
+    print("Loading", len(args.rsem_results), "RSEM files")
+    sample_counts, tx_lengths = aggregate_rsem(args.rsem_results)
+
+    sample_counts = rna.filter_probes(sample_counts)
+    # DBG
+    if args.output:
+        sample_counts.to_csv(args.output + ".sample_counts.tsv",
+                             sep='\t', index=True)
+        print("Wrote", args.output + ".sample_counts.tsv",
+              "with", len(sample_counts), "rows")
+
+    print("Loading gene metadata",
+          "and TCGA gene expression/CNV profiles"
+          if args.tcga_cnv and args.tcga_expression
+          else "")
+    gene_info = rna.load_gene_info(args.gene_resource,
+                                   args.tcga_cnv, args.tcga_expression)
+
+    print("Aligning gene info to sample gene counts")
+    gene_info, sample_counts, sample_depths_log2 = rna.align_gene_info_to_samples(
+        gene_info, sample_counts, tx_lengths)
+
+    print("Writing output files")
+    # Summary table has log2-normalized values, not raw counts
+    # ENH show both, with column header suffixes to distinguish?
+    all_data = pd.concat([gene_info, sample_depths_log2], axis=1)
+    if args.output:
+        all_data.to_csv(args.output, sep='\t', index=True)
+        print("Wrote", args.output, "with", len(all_data), "rows")
+    else:
+        print(all_data.describe(), file=sys.stderr)
+
+    if args.cnr_dir:
+        # CNVkit files have both absolute and log2-normalized read counts
+        cnrs = rna.attach_gene_info_to_cnr(sample_counts, sample_depths_log2,
+                                           gene_info)
+        for cnr in cnrs:
+            outfname = os.path.join(args.cnr_dir, cnr.sample_id + ".cnr")
+            cnr = rna.correct_cnr(cnr)
+            tabio.write(cnr, outfname, 'tab')
+
+
+
+if __name__ == '__main__':
+    import argparse
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
+
+    AP = argparse.ArgumentParser(description=__doc__)
+    AP.add_argument("rsem_results", nargs='+',
+                    help="RSEM output files (*.results)")
+    AP.add_argument("-o", "--output", metavar="FILE",
+                    help="Output file name (summary table).")
+    AP.add_argument("--cnr-dir", metavar="PATH",
+                    help="""Directory to write a CNVkit .cnr file for each input
+                    sample.""")
+    AP.add_argument("-g", "--gene-resource", metavar="FILE",
+                    help="Location of gene info table from BioMart.")
+
+    AP_tcga = AP.add_argument_group(
+        "To correlate each gene's copy number with expression")
+    AP_tcga.add_argument("--tcga-cnv", metavar="CNV_FILE",
+                    help="""CNVs for many TCGA samples, downloaded from
+                    cBioPortal.""")
+    AP_tcga.add_argument("--tcga-expression", metavar="RNA_FILE",
+                    help="""Gene expression for many TCGA samples, downloaded from
+                    cBioPortal.""")
+
+    main(AP.parse_args())
