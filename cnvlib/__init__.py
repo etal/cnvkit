f1e19ca97940fd1a739305bd27fdc343a9acd5cd
Eric Talevich <eric.talevich@gmail.com>
2018-01-12 09:35:43 -0800

    cnvlib: Only import tabio.write to top level, not tabio itself
    This way, 'cnvlib.write(cnarr, "foo.cnr")' works as expected (default
    'tab' format), writing CNVkit's native format without thinking about
    tabio at all, symmetric with 'cnvlib.read("foo.cnr")'.
    
    To read other formats with tabio, the user must work with skgenome
    directly.

--- cnvlib/__init__.py
+++ cnvlib/__init__.py
@@ -1,5 +1,6 @@
+from skgenome.tabio import write
+
 from ._version import __version__
 from .cmdutil import read_cna as read
 from .commands import *
 from .diagram import create_diagram as do_diagram
-from skgenome import tabio
