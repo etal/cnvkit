329549bdbbe0caefddf68f8bdba90ac8953116a3
Eric Talevich <etalevich@dnanexus.com>
2019-03-20 20:53:40 -0700

    segmentation: Reimplement HMM methods in pomegranate

--- cnvlib/segmentation/hmm.py
+++ cnvlib/segmentation/hmm.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
 """Segmentation by Hidden Markov Model."""
 from __future__ import absolute_import, division, print_function
+import collections
 import logging
 
 import numpy as np
 import pandas as pd
-#  from hmmlearn import hmm
-from pomegranate import *
+import scipy.special
+import pomegranate as pom
+
+from ..descriptives import biweight_midvariance
+from ..segfilters import squash_by_groups
 
 
 def segment_hmm(cnarr, method, window=None, processes=1):
@@ -33,58 +37,31 @@ def segment_hmm(cnarr, method, window=None, processes=1):
     # NB: Incorporate weights into smoothed log2 estimates
     # (Useful kludge until weighted HMM is in place)
     orig_log2 = cnarr['log2'].values.copy()
-    cnarr['log2'] = cnarr.smoothed(window)
+    cnarr['log2'] = cnarr.smoothed()#window)
 
-    logging.debug("Building model from observations")
-    model = hmm_get_model_DEMO(cnarr, method, processes)
-    logging.debug("Predicting states from model")
+    logging.info("Building model from observations")
+    model = hmm_get_model(cnarr, method, processes)
 
-    #  obs = as_observation_matrix(cnarr)
-    obs = [arm.log2 for _c, arm in cnarr.by_arm()]
+    logging.info("Predicting states from model")
+    observations = as_observation_matrix(cnarr)
+    states = np.concatenate([np.array(model.predict(obs, algorithm='map'))
+                             for obs in observations])
 
-    # A sequence of inferred discrete states. Length is the same as `freqs`,
-    # with one state assigned to each input datapoint.
-    #  states = model.predict(obs, lengths=chrom_arm_lengths(cnarr))
-    #  _ll, states = model.viterbi(obs)
-    #  _ll, states = model.maximum_a_posteriori(obs)  # the default
-
-    states = np.concatenate([np.array(model.predict(o, algorithm='map'))
-                             for o in obs])
-    # XXX this is all -1?
-
-    # TODO logging.warn if model did not converge
-    # print(model, end="\n\n")
-    # print("Model params:\nmeans =", sorted(model.means_.flat))
-    # print(model.monitor_, end="\n\n")
+    logging.info("Done, now finalizing")
+    logging.debug("Model states: %s", model.states)
+    logging.debug("Predicted states: %s", states[:100])
+    logging.debug(str(collections.Counter(states)))
+    logging.debug("Observations: %s", observations[0][:100])
+    logging.debug("Edges: %s", model.edges)
 
     # Merge adjacent bins with the same state to create segments
-    from ..segfilters import squash_by_groups
     cnarr['log2'] = orig_log2
     cnarr['probes'] = 1
     segarr = squash_by_groups(cnarr, pd.Series(states), by_arm=True)
     return segarr
 
 
-def hmm_get_model_DEMO(cnarr, method, processes):
-
-    cnarr = cnarr.autosomes()
-    #  freqs = as_observation_matrix(cnarr)
-    freqs = [arm.log2 for _c, arm in cnarr.by_arm()]
-
-    model = HiddenMarkovModel.from_samples(NormalDistribution,
-                                           n_components=3,
-                                           X=freqs,
-                                           min_iterations=2,
-                                           max_iterations=100,
-                                           # XXX can use processes=_ here
-                                           n_jobs=processes,
-                                           verbose=False, # For testing
-                                          )
-    # TODO - set fixed state means for germline
-    return model
-
-
-def hmm_get_model(cnarr, method):
+def hmm_get_model(cnarr, method, processes):
     """
 
     Parameters
@@ -92,61 +69,94 @@ def hmm_get_model(cnarr, method):
     cnarr : CopyNumArray
         The normalized bin-level values to be segmented.
     method : string
-        One of 'hmm', 'hmm-tumor', 'hmm-germline'
+        One of 'hmm', 'hmm-tumor', 'hmm-germline'.
+    processes : int
+        Number of parallel jobs to run.
 
     Returns
     -------
     model :
-        A hmmlearn Model trained on the given dataset.
-
+        A pomegranate HiddenMarkovModel trained on the given dataset.
     """
-    #  import warnings
-    #  warnings.simplefilter('ignore', DeprecationWarning)
-
     assert method in ('hmm-tumor', 'hmm-germline', 'hmm')
-    if method == 'hmm-tumor':
-        state_means = np.column_stack([[-4, -1, 0, .585, 1]])
-    else:
-        state_means = np.column_stack([[-1, 0, .585]])
-    kwargs = {
-        'n_components': len(state_means),
-        'means_prior': state_means,
-        'means_weight': .5,
-    }
+    observations = as_observation_matrix(cnarr.autosomes())
+
+    # Estimate standard deviation from the full distribution, robustly
+    stdev = biweight_midvariance(np.concatenate(observations), initial=0)
     if method == 'hmm-germline':
-        # Don't alter the state means when training the model
-        kwargs.update({
-            'init_params': 'stc',
-            'params': 'stc',
-        })
-    model = hmm.GaussianHMM(covariance_type='diag', n_iter=100,
-                            random_state=0xA5EED, **kwargs)
-    if method == 'germline':
-        model.means_ = state_means
-
-    # Create observation matrix
-    # TODO incorporate weights -- currently handled by smoothing
-    # TODO incorporate inter-bin distances
-    cnarr = cnarr.autosomes()
-    freqs = as_observation_matrix(cnarr)
-
-    if cnarr.chromosome.nunique() == 1:
-        model.fit(freqs, algorithm='baum-welch', verbose=False)
+        state_names = ["loss", "neutral", "gain"]
+        distributions = [
+            pom.NormalDistribution(-1.0, stdev, frozen=True),
+            pom.NormalDistribution(0.0, stdev, frozen=True),
+            pom.NormalDistribution(0.585, stdev, frozen=True),
+        ]
+    elif method == 'hmm-tumor':
+        state_names = ["del", "loss", "neutral", "gain", "amp"]
+        distributions = [
+            pom.NormalDistribution(-2.0, stdev, frozen=False),
+            pom.NormalDistribution(-0.5, stdev, frozen=False),
+            pom.NormalDistribution(0.0, stdev, frozen=True),
+            pom.NormalDistribution(0.3, stdev, frozen=False),
+            pom.NormalDistribution(1.0, stdev, frozen=False),
+        ]
     else:
-        model.fit(freqs, lengths=chrom_arm_lengths(cnarr), verbose=False)
+        state_names = ["loss", "neutral", "gain"]
+        distributions = [
+            pom.NormalDistribution(-1.0, stdev, frozen=False),
+            pom.NormalDistribution(0.0, stdev, frozen=False),
+            pom.NormalDistribution(0.585, stdev, frozen=False),
+        ]
+
+    n_states = len(distributions)
+    # Starts -- prefer neutral
+    binom_coefs = scipy.special.binom(n_states - 1, range(n_states))
+    start_probabilities = binom_coefs / binom_coefs.sum()
+    # Ends -- equally likely
+    #end_probabilities = np.ones(n_states) / n_states
+
+    # Prefer to keep the current state in each transition
+    # All other transitions are equally likely, to start
+    transition_matrix = (np.identity(n_states) * 100
+                        + np.ones((n_states, n_states)) / n_states)
+
+    model = pom.HiddenMarkovModel.from_matrix(transition_matrix, distributions,
+        start_probabilities, state_names=state_names, name=method)
+
+    model.fit(sequences=observations,
+              weights=[len(obs) for obs in observations],
+              distribution_inertia = .8,  # Allow updating dists, but slowly
+              edge_inertia=0.1,
+              # lr_decay=.75,
+              pseudocount=5,
+              use_pseudocount=True,
+              max_iterations=100000,
+              n_jobs=processes,
+              verbose=False)
     return model
 
 
-def as_observation_matrix(cnarr):
-    columns = [cnarr['log2'].values]
-    if 'baf' in cnarr:
-        # XXX untested
-        columns.append(cnarr['baf'].values)
-    return np.column_stack(columns)
-    return columns
+def as_observation_matrix(cnarr, variants=None):
+    """Extract HMM fitting values from `cnarr`.
 
+    For each chromosome arm, extract log2 ratios as a numpy array.
 
-def chrom_arm_lengths(cnarr):
-    lengths = [len(x) for _, x in cnarr.by_arm()]
-    assert sum(lengths) == len(cnarr)
-    return lengths
+    Future: If VCF of variants is given, or 'baf' column has already been
+    added to `cnarr` from the same, then the BAF values are a second row/column
+    in each numpy array.
+
+    Returns: List of numpy.ndarray, one per chromosome arm.
+    """
+    # TODO incorporate weights -- currently handled by smoothing
+    # TODO incorporate inter-bin distances
+    observations = [arm.log2.values
+                    for _c, arm in cnarr.by_arm()]
+    return observations
+    # --- TODO incorporate variant BAF/zygosity/LOH/ROH ---
+    if variants is not None:
+        print("Variants!")
+        segarr['baf'] = variants.baf_by_ranges(segarr)
+    elif 'baf' in cnarr:
+        print("BAF!")
+        observations = [np.hstack([arm.log2.values, arm['baf'].values])
+                        for _c, arm in cnarr.by_arm()]
+    # /---
