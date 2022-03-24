2237728ce8375822e03f9e31aeb32fb9a9bdfb90
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-25 00:15:25 +0800

    Delete cnvlib directory

--- cnvlib/__init__.py
+++
@@ -1,6 +0,0 @@
-from skgenome.tabio import write
-
-from ._version import __version__
-from .cmdutil import read_cna as read
-from .commands import *
-from .diagram import create_diagram as do_diagram
