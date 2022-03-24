9f83556c5ce599bb224eee9c972404f718978c07
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-25 01:15:26 +0800

    Delete cnvkit.py

--- cnvkit.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Command-line interface for CNVkit, the Copy Number Variation toolkit."""
-import logging
-from cnvlib import commands
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format="%(message)s")
-    args = commands.parse_args()
-    args.func(args)
