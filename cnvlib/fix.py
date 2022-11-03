409ad1e2d7c73ad4554934311e765fbc184ef28a
Eric Talevich <etalevich@ucsf.edu>
2022-11-02 22:12:51 -0700

    fix: Choose a smoothing window fraction based on the data size (#379)
    Previously it was fixed at 0.1; now it's 1/sqrt(length), down to a limit of 0.01.
    
    Behavior is unchanged for a vector length of 100. For smaller vectors, e.g.
    down to ~20 as could be seen in the test file amplicon.cnr, the smoothing window
    is wider (~.22, or 4 data points), and for larger vectors shrinks down to a
    limit of 0.01 for vector length >= 10000 (for >=100 data points in the window).
    
    Compare to GC-binning functionality in Picard tools and GATK, where a similar
    adjustment is made at each percentile rank, equivalent to a fixed window
    fraction of .01 but without rolling/smoothing.
    
    This should correct biases better at the extremes of the GC range in particular
    (when there are enough data points), where previously the fitted line would
    flatten out at the edges due to the wide smoothing window.

--- cnvlib/fix.py
+++ cnvlib/fix.py
@@ -82,23 +82,25 @@ def load_adjust_coverages(cnarr, ref_cnarr, skip_low,
         logging.warning("WARNING: most bins have no or very low coverage; "
                         "check that the right BED file was used")
     else:
+        # Smoothing window fraction converges on percentile binning, like Picard
+        frac = max(.01, len(cnarr) ** -.5)
         cnarr_index_reset = False
         if fix_gc:
             if 'gc' in ref_matched:
                 logging.info("Correcting for GC bias...")
-                cnarr = center_by_window(cnarr, .1, ref_matched['gc'])
+                cnarr = center_by_window(cnarr, frac, ref_matched['gc'])
                 cnarr_index_reset = True
             else:
                 logging.warning("WARNING: Skipping correction for GC bias")
         if fix_edge:
             logging.info("Correcting for density bias...")
             edge_bias = get_edge_bias(cnarr, params.INSERT_SIZE)
-            cnarr = center_by_window(cnarr, .1, edge_bias)
+            cnarr = center_by_window(cnarr, frac, edge_bias)
             cnarr_index_reset = True
         if fix_rmask:
             if 'rmask' in ref_matched:
                 logging.info("Correcting for RepeatMasker bias...")
-                cnarr = center_by_window(cnarr, .1, ref_matched['rmask'])
+                cnarr = center_by_window(cnarr, frac, ref_matched['rmask'])
                 cnarr_index_reset = True
             else:
                 logging.warning("WARNING: Skipping correction for "
