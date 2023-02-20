ea7066b42fa15bd505de105afa8960eb067d6bd0
Rolf Schröder <rolf.schroeder@limbus-medtec.com>
2023-02-20 12:40:48 +0100

    Remove get_copied_raw_dataframe().

--- skgenome/gary.py
+++ skgenome/gary.py
@@ -95,9 +95,6 @@ class GenomicArray(object):
             dframe = dframe.reset_index(drop=True)
         return self.__class__(dframe, self.meta.copy())
 
-    def get_copied_raw_dataframe(self):
-        return self.data.copy()
-
     def as_series(self, arraylike):
         return pd.Series(arraylike, index=self.data.index)
 
