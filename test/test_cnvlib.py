41dfc80e1f762757d4ab08ce85dba96a9d9271fd
Eric Talevich <etal@users.noreply.github.com>
2023-02-23 22:11:46 -0800

    test: Apply black and pylint to test runner scripts

--- test/test_cnvlib.py
+++ test/test_cnvlib.py
@@ -3,18 +3,20 @@
 import unittest
 
 import logging
+
 logging.basicConfig(level=logging.ERROR, format="%(message)s")
 
 # unittest/pomegranate 0.10.0: ImportWarning: can't resolve package from
 # __spec__ or __package__, falling back on __name__ and __path__
 import warnings
-warnings.filterwarnings('ignore', category=ImportWarning)
+
+warnings.filterwarnings("ignore", category=ImportWarning)
 
 import numpy as np
 from skgenome import GenomicArray, tabio
 
 import cnvlib
-from cnvlib import (cnary, core, fix, smoothing, vary)
+from cnvlib import cnary, core, fix, smoothing, vary
 
 
 class CNATests(unittest.TestCase):
@@ -27,12 +29,11 @@ class CNATests(unittest.TestCase):
 
     def test_basic(self):
         """Test basic container functionality and magic methods."""
-        cna = cnvlib.read('formats/reference-tr.cnn')
+        cna = cnvlib.read("formats/reference-tr.cnn")
         # Length
-        self.assertEqual(len(cna),
-                         linecount('formats/reference-tr.cnn') - 1)
+        self.assertEqual(len(cna), linecount("formats/reference-tr.cnn") - 1)
         # Equality
-        same = cnvlib.read('formats/reference-tr.cnn')
+        same = cnvlib.read("formats/reference-tr.cnn")
         self.assertEqual(cna, same)
         # Item access
         orig = cna[0]
@@ -44,48 +45,50 @@ class CNATests(unittest.TestCase):
 
     def test_center_all(self):
         """Test recentering."""
-        cna = cnvlib.read('formats/reference-tr.cnn')
+        cna = cnvlib.read("formats/reference-tr.cnn")
         # Median-centering an already median-centered array -> no change
-        chr1 = cna.in_range('chr1')
-        self.assertAlmostEqual(0, np.median(chr1['log2']), places=1)
+        chr1 = cna.in_range("chr1")
+        self.assertAlmostEqual(0, np.median(chr1["log2"]), places=1)
         chr1.center_all()
-        orig_chr1_cvg = np.median(chr1['log2'])
+        orig_chr1_cvg = np.median(chr1["log2"])
         self.assertAlmostEqual(0, orig_chr1_cvg)
         # Median-centering resets a shift away from the median
         chr1plus2 = chr1.copy()
-        chr1plus2['log2'] += 2.0
+        chr1plus2["log2"] += 2.0
         chr1plus2.center_all()
-        self.assertAlmostEqual(np.median(chr1plus2['log2']), orig_chr1_cvg)
+        self.assertAlmostEqual(np.median(chr1plus2["log2"]), orig_chr1_cvg)
         # Other methods for centering are similar for a CN-neutral chromosome
         for method in ("mean", "mode", "biweight"):
             cp = chr1.copy()
             cp.center_all(method)
-            self.assertLess(abs(cp['log2'].median() - orig_chr1_cvg), 0.1)
+            self.assertLess(abs(cp["log2"].median() - orig_chr1_cvg), 0.1)
 
     def test_drop_extra_columns(self):
         """Test removal of optional 'gc' column."""
-        cna = cnvlib.read('formats/reference-tr.cnn')
-        self.assertIn('gc', cna)
+        cna = cnvlib.read("formats/reference-tr.cnn")
+        self.assertIn("gc", cna)
         cleaned = cna.drop_extra_columns()
-        self.assertNotIn('gc', cleaned)
-        self.assertTrue((cleaned['log2'] == cna['log2']).all())
+        self.assertNotIn("gc", cleaned)
+        self.assertTrue((cleaned["log2"] == cna["log2"]).all())
 
     def test_guess_xx(self):
         """Guess chromosomal sex from chrX log2 ratio value."""
         for (fname, sample_is_f, ref_is_m) in (
-                ("formats/f-on-f.cns", True, False),
-                ("formats/f-on-m.cns", True, True),
-                ("formats/m-on-f.cns", False, False),
-                ("formats/m-on-m.cns", False, True),
-                ("formats/amplicon.cnr", False, True),
-                ("formats/cl_seq.cns", True, True),
-                ("formats/tr95t.cns", True, True),
-                ("formats/reference-tr.cnn", False, False),
-            ):
+            ("formats/f-on-f.cns", True, False),
+            ("formats/f-on-m.cns", True, True),
+            ("formats/m-on-f.cns", False, False),
+            ("formats/m-on-m.cns", False, True),
+            ("formats/amplicon.cnr", False, True),
+            ("formats/cl_seq.cns", True, True),
+            ("formats/tr95t.cns", True, True),
+            ("formats/reference-tr.cnn", False, False),
+        ):
             guess = cnvlib.read(fname).guess_xx(ref_is_m)
-            self.assertEqual(guess, sample_is_f,
-                             "%s: guessed XX %s but is %s"
-                             % (fname, guess, sample_is_f))
+            self.assertEqual(
+                guess,
+                sample_is_f,
+                f"{fname}: guessed XX {guess} but is {sample_is_f}",
+            )
 
     def test_residuals(self):
         cnarr = cnvlib.read("formats/amplicon.cnr")
@@ -93,15 +96,17 @@ class CNATests(unittest.TestCase):
         regions = GenomicArray(segments.data).drop_extra_columns()
         for grouping_arg in (None, segments, regions):
             resid = cnarr.residuals(grouping_arg)
-            self.assertAlmostEqual(0, resid.mean(), delta=.3)
-            self.assertAlmostEqual(1, np.percentile(resid, 80), delta=.2)
-            self.assertAlmostEqual(2, resid.std(), delta=.5)
+            self.assertAlmostEqual(0, resid.mean(), delta=0.3)
+            self.assertAlmostEqual(1, np.percentile(resid, 80), delta=0.2)
+            self.assertAlmostEqual(2, resid.std(), delta=0.5)
 
     def test_smooth_log2(self):
-        for fname in ["formats/amplicon.cnr",
-                      "formats/wgs-chr17.cnr",
-                      "formats/p2-20_1.cnr",
-                      "formats/p2-20_2.cnr"]:
+        for fname in [
+            "formats/amplicon.cnr",
+            "formats/wgs-chr17.cnr",
+            "formats/p2-20_1.cnr",
+            "formats/p2-20_2.cnr",
+        ]:
             cnarr = cnvlib.read(fname)
             orig_vals = cnarr.log2.values.copy()
             signal = cnarr.smooth_log2()
@@ -128,11 +133,14 @@ class OtherTests(unittest.TestCase):
         target_size = np.array([450])
         self.assertAlmostEqual(
             fix.edge_losses(target_size, insert_size),
-            2 * fix.edge_gains(target_size, gap_size, insert_size))
+            2 * fix.edge_gains(target_size, gap_size, insert_size),
+        )
         # Trigger 'gain' correction (target_size + gap_size < insert_size)
         target_size = np.array([300])
-        self.assertAlmostEqual(fix.edge_losses(target_size, insert_size),
-                        2 * fix.edge_gains(target_size, gap_size, insert_size))
+        self.assertAlmostEqual(
+            fix.edge_losses(target_size, insert_size),
+            2 * fix.edge_gains(target_size, gap_size, insert_size),
+        )
 
     # call
     # Test: convert_clonal(x, 1, 2) == convert_diploid(x)
@@ -149,6 +157,7 @@ class VATests(unittest.TestCase):
 
 # == helpers ==
 
+
 def linecount(filename):
     i = -1
     with open(filename) as handle:
@@ -157,5 +166,5 @@ def linecount(filename):
         return i + 1
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(verbosity=2)
