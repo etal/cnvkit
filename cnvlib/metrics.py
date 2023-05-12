f128359ccec148b6329a8586b8e71457277a4d32
Rolf Schröder <rolf.schroeder@limbus-medtec.com>
2023-05-12 11:45:26 +0200

    Remove comment.

--- cnvlib/metrics.py
+++ cnvlib/metrics.py
@@ -6,7 +6,7 @@ import pandas as pd
 from . import descriptives
 
 
-def do_metrics(cnarrs, segments=None, skip_low=False): # todo: make this work in parallel?
+def do_metrics(cnarrs, segments=None, skip_low=False):
     """Compute coverage deviations and other metrics for self-evaluation."""
     # Catch if passed args are single CopyNumArrays instead of lists
     from .cnary import CopyNumArray as CNA
